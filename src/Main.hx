62872721bea354f12b717e75d688a1d1c5a346dc
drflamemontgomery <drflamemontgomery@gmail.com>
2023-02-19 17:29:00 +1300

    modified test string\nimplemented print statements of implemented data types

--- src/Main.hx
+++ src/Main.hx
@@ -18,17 +18,47 @@ class Main {
     switch(expr) {
       case LexNull:
         return 'null';
+
       case LexBool(t):
         return '$t';
+
       case LexNumber(n):
         return '$n';
+
+      case LexString(s):
+        return '"$s"';
+
+      case LexArray(arr):
+        var str = [ for(i in arr) exprToString(i) ];
+        return '$str';
+
+      case LexCall(name, args):
+        return '$name $args';
+
+      case LexFunction(type, name, args, body):
+        switch(type) {
+          case ConstantVariable:
+            return 'let $name = ${ (body.length == 0 ? 'void' : exprToString(body[0]) ) }';
+          case NonConstantVariable:
+            return 'set $name = ${ (body.length == 0 ? 'void' : exprToString(body[0]) ) }';
+          case Function:
+            return 'Function not implemented';
+        }
+
       default:
         return 'Undefined';
     }
   }
 
   public static function main() {
-    final test_string = "   let set";
+    final test_string = "
+let one = -12.32\n
+set two = true\n
+let a = [[123, 234], []]
+let b = 'test'
+//let a = { a : 1.12 }
+//let b = 123
+";
 
     Lambda.map(parser.Lexer.parse(test_string + '\n'), printExpr);
   }
