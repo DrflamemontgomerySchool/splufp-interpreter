c14dbfec0b4085449445a6041ad44b8f2543f285
drflamemontgomery <drflamemontgomery@gmail.com>
2023-02-22 18:41:42 +1300

    Added useful functions

--- spl-extern/splufp-lang.js
+++ spl-extern/splufp-lang.js
@@ -49,3 +49,48 @@ function obj_get(obj, name) {
 function array_at(arr, n) {
   return arr[n];
 }
+
+function slice(arr, pos, end) {
+  return arr.slice(pos, end);
+}
+
+function splice(arr, pos, len) {
+  return arr.splice(pos, len);
+}
+
+
+
+function len(arr) {
+  return arr.length;
+}
+
+function foldr(fn, init, arr) {
+  if(arr.length > 1) {
+    return foldr(fn, fn(new __splufp__function(init))(new __splufp__function(arr[0])), arr.slice(1, arr.length));
+  }
+  else if(arr.length > 0) {
+    return fn(new __splufp__function(init))(new __splufp__function(arr[0]));
+  }
+  return init;
+}
+
+function foldl(fn, init, arr) {
+  if(arr.length > 1) {
+    return foldl(fn, fn(new __splufp__function(arr[0]))(new __splufp__function(init)), arr.slice(1, arr.length));
+  }
+  else if(arr.length > 0) {
+    return fn(new __splufp__function(arr[0]))(new __splufp__function(init));
+  }
+  return init;
+}
+
+function map(fn, arr) {
+  if(arr.length > 0) {
+    fn(new __splufp__function(arr[0]));
+    map(fn, arr.slice(1, arr.length));
+  }
+}
+
+function log(str) {
+  console.log(str);
+}
